30b392af9f7cd3fc12609914b68fa6893e14c4e4
DanDHR <98965144+DanDHR@users.noreply.github.com>
2025-03-31 11:31:20 +0000

    Update Main.py
    Added a bit more documentation

--- Main.py
+++ Main.py
@@ -6,6 +6,7 @@ from DistributionCalc import DistributionCalc
 from Phylogeny import PhylogeneticTreeBuilder
 import matplotlib.pyplot as plt
 
+
 def fasta_file_reader(file_path) -> list:
     """
     Reads a fasta file and returns all sequences within it as a list
@@ -15,7 +16,6 @@ def fasta_file_reader(file_path) -> list:
 
     Return:
         - list: list of the sequences within the read file
-
     """
     #Read the file and set its type as a list
     record = list(SeqIO.parse(file_path, "fasta"))
@@ -35,7 +35,6 @@ def find_best_pairwise_alignment(target_record, breeds_record, difference = "") 
             !!!The default is set to not calculate this, since it takes too much recourses and it takes a long time to process that!!!
     Return:
         - list: a list of all the alignment scores between each of the breeds records and the targeted record
-    
     """
     #Initialise PairwiseAlignment class. It will save an instance of each of those records
     pa = PairwiseAlignment(target_record, breeds_record)
@@ -56,27 +55,36 @@ def find_best_pairwise_alignment(target_record, breeds_record, difference = "") 
 
     return pa.scores_get()      #Return the scores of all alignments. It is needed later.
 
+
 def records_distribution(target_record, breeds_record, scores) ->list:
     """
-    
+    Calculate the distribution of sequence scores from sequence alignments
+
     Args:
+        - target_record (str): the record that we are interested in
+        - breeds_record (list[str,...]): a list of sequences from dog breeds
+        - scores (list[float, ...]): the scores of the pairwise alignments between the target record and each of the breeds records
 
     Return:
-    
+        -list: for each score, calculate its p-value    
     """
-    distr = DistributionCalc(target_record, breeds_record, scores)
-    return distr.p_values_get()
-
+    distr = DistributionCalc(target_record, breeds_record, scores)          #Initialise the DistributionCalc to calculate the p-values from the scores
+    return distr.p_values_get()                                             #Return the p-values
 
 
 if __name__ == '__main__':
-
+    #Load the target and the list of dog breeds dna sequences
     breeds_records = fasta_file_reader("project_dog_dna/dog_breeds.fa")
     target_record = fasta_file_reader("project_dog_dna/mystery.fa")
+    #Find the best pairwise alignment and get their scores
+    scores = find_best_pairwise_alignment(target_record[0],breeds_records)          #Main goal
 
-    scores = find_best_pairwise_alignment(target_record[0],breeds_records)
-    print(records_distribution(target_record[0], breeds_records, scores))
 
+    #Calculate the distribution of sequence scores from sequence alignments
+    print(records_distribution(target_record[0], breeds_records, scores))           #Stretch goal 1
+
+
+    #Load a Multiple Sequence Alignment
     alignment = AlignIO.read("external_data/dog_breeds_alignment.fa", "fasta")
-    phylo_tree = PhylogeneticTreeBuilder(alignment)
-
+    #Create a phylogenetic tree
+    phylo_tree = PhylogeneticTreeBuilder(alignment)                                 #Stretch goal 2
