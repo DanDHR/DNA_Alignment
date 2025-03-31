7d13c564d895fce3ceca57154f63c4c1eb4890ef
DanDHR <98965144+DanDHR@users.noreply.github.com>
2025-03-31 11:32:19 +0000

    Update DistributionCalc.py
    Added more documentation

--- DistributionCalc.py
+++ DistributionCalc.py
@@ -8,7 +8,7 @@ from scipy.stats import genextreme
 
 class DistributionCalc(unittest.TestCase):
     """
-    A class with the purpose of making pairwise alignments between a single sequence and each sequence withing a list of sequences
+    A class with the purpose of calculating the distribution of sequence scores from sequence alignments
     
     Object attributes:
         - target_record (str): the targeted sequence, each alignment would be done with respect to it
@@ -18,7 +18,7 @@ class DistributionCalc(unittest.TestCase):
 
     Object methods:
         - __init__(self, graph): Create a DistributionCalc object
-        - fit_gumbel_distribution(self): Computes the Lambda.
+        - fit_gumbel_distribution(self): Initialises both the lambda and K constant values.
         - compute_e_value(self, score, lambda_, K): Computes the E value.
         - compute_p_value(self, e_value): Computes a single p_value.
         - compute_p_values(self): Computes the p_value from provided scores list.
@@ -26,41 +26,80 @@ class DistributionCalc(unittest.TestCase):
     """
 
     def __init__(self, target_record, breeds_record, scores):
-
+        #We need both records only for their lengths
         self.target_record = target_record
         self.breeds_record = breeds_record
-
+        #Get the scores of all alignments
         self.scores = scores
+        #Initialise a place holder array for p-values
         self.p_values = np.zeros(len(scores))
-
+        #Compute all the p-values
         self.compute_p_values()
 
 
-    def fit_gumbel_distribution(self):
-        """ Computes the Lambda. """
-        shape, loc, scale = genextreme.fit(-self.scores) 
+    def fit_gumbel_distribution(self) -> tuple[float, float]:
+        """ 
+        Initialises both the lambda and K constant values. 
+        """
+        shape, loc, scale = genextreme.fit(-self.scores)    
+
+        #Compute lambda
         lambda_const = 1 / scale 
-        K = 0.1  
+
+        #Set K
+        K = 0.1                 
         return lambda_const, K
     
 
-    def compute_e_value(self, score, lambda_, K):
-        """ Computes the E value. """
-        M, N = len(self.target_record.seq), len(self.breeds_record[0].seq)  # Sequence lengths
-        return K * M * N * np.exp(-lambda_ * score)
+    def compute_e_value(self, score, lambda_, K) -> float:
+        """ 
+        Computes the E value.
+
+        Args:
+            - score (float): the provided score for an alignment
+            - lambda_ (float): lambda constant 
+            - K (float): K constant
+            
+        Return:
+            - float: the computed E value
+        
+        """
+        M, N = len(self.target_record.seq), len(self.breeds_record[0].seq)  #Sequence lengths
+        return K * M * N * np.exp(-lambda_ * score)                         #Return the E value
     
 
-    def compute_p_value(self, e_value):
-        """ Computes a single p_value. """
+    def compute_p_value(self, e_value) -> float:
+        """ 
+        Computes a single p_value.
+
+        Args:
+            - e_value (float): the previously computed E value
+
+        Return:
+            - float: the coputed p-value
+        
+        """
         return 1 - np.exp(-e_value)
     
 
-    def compute_p_values(self):
-        """ Computes the p_value from provided scores list. """
-        lambda_const, K = self.fit_gumbel_distribution()
+    def compute_p_values(self) -> None:
+        """ 
+        Computes the p_values from provided scores list. 
+        """
+
+        lambda_const, K = self.fit_gumbel_distribution()                            #Initialise lambda and K constants
+
+        #iterate through all scores
         for i, score in enumerate(self.scores):
-            e_value = self.compute_e_value(score, lambda_const, K)
-            self.p_values[i] = self.compute_p_value(e_value)
+            e_value = self.compute_e_value(score, lambda_const, K)                  #Calculate the E value
+            self.p_values[i] = self.compute_p_value(e_value)                        #Calculate the p-value
+
+
+    def p_values_get(self) -> float:
+        """ 
+        Returns the calculated p value. 
+        """
+        return self.p_values
 
 
     def p_values_get(self):
